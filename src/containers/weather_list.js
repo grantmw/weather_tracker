84abc02fe4e7dc5888809847366b870567935a24
grantmw <grantmwong@gmail.com>
2016-11-30 13:55:31 -0800

    implement google map, replace name

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/map.js';
 
 
 class WeatherList extends Component {
@@ -11,13 +12,17 @@ class WeatherList extends Component {
 		const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => (temp*(9/5))-459.67);
 		const pressures = cityData.list.map(weather => weather.main.pressure);
 		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const lat = cityData.city.coord.lat;
+		const lon = cityData.city.coord.lon;
+
+
 		return (
 			<tr key={name}>
 				<td>
-					{name}
+					<GoogleMap lon={lon} lat={lat} />
 				</td>
 				<td>
-					<Chart data={temps} color='red' units='K' />
+					<Chart data={temps} color='red' units='&deg;F' />
 				</td>
 				<td>
 					<Chart data={pressures} color='yellow' units='hPa' />
